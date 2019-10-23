3d4c8b6a189239696a2f6b7f6d99f2d75cc3690d
Abey Onalaja <abey.onalaja@gmail.com>
2019-10-23 13:21:53 -0500

    project init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+rltk::add_wasm_support!();
+use rltk::{Rltk, GameState, Console};
+struct State {}
+impl GameState for State{
+    fn tick(&mut self, ctx : &mut Rltk){
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() {
+    let context = Rltk::init_simple8x8(80, 50, "grrr", "resources");
+    let gs = State{ };
+    rltk::main_loop(context, gs);
+}
